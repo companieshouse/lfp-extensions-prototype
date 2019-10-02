25dad68c55d4dfc2b8790abed1e807ba174c1578
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2019-10-02 11:05:03 +0100

    far scenario commit

---
+++ app/assets/scenarios/00111222.js
@@ -0,0 +1,16 @@
+module.exports = {
+  company: {
+    name: 'LUCKY CHALMERS LIMITED',
+    number: '00111222',
+    authCode: 'LCL00111222',
+    status: 'Active',
+    incorporationDate: '25 May 2016',
+    companyType: 'Private Limited Company',
+    address: '25 Gold Lane\nNewport\nNP1 2SS',
+    accountsDue: '4 November 2019',
+    extensionOneWeek: '11 October 2019',
+    extensionTwoWeek: '18 November 2019',
+    extensionFourWeek: '21 November 2018',
+    deadlineStatus: 'Far'
+  }
+}
